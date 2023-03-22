1d41d4c26d51a748e412d8a760f075f2e79c4837
mariana27oliveira <marianarodriguesoliveira2001@gmail.com>
2023-03-22 23:35:20 +0000

    tpc4

--- aula4/file2.py
+++ aula4/file2.py
@@ -1,51 +1,29 @@
-'''
-#VERSÃO USANDO LISTA DE LISTAS NO JSON
 import json
 import re
 
-file=open("dicionario_medico_aula.json", encoding="utf-8")
-lista = json.load(file)
-
-#json nao tem tuples então ficamos com uma lista de listas
-
-
-file1=open("LIVRO-Doenças-do-Aparelho-Digestivos.html", "r+", encoding="utf-8")
-texto = file1.read()
-for [termo, definição] in lista:
-    if termo in texto:
-        texto = re.sub(termo, f'<a href="#{termo}">{definição}</a>', texto)
-
-print("\nTexto modificado:\n", texto)
-file1.write(texto)
-file1.flush() 
-file1.close()
-'''
-
-#VERSÃO USANDO DICIONÁRIO
-import json
-import re
-
-with open("dicionario_medico_aula.json", encoding="utf-8") as file:
+#with open("dicionario_medico_aula.json", encoding="utf-8") as file:
+with open("dicionario_medico_aula_teste.json", encoding="utf-8") as file:
     dicionario = json.load(file)
 
-with open("LIVRO-Doenças-do-Aparelho-Digestivos.html", "r+", encoding="utf-8") as file1:
+#with open("LIVRO-Doenças-do-Aparelho-Digestivos.html", "r+", encoding="utf-8") as file1:
+with open("LIVRO-Doenças-do-Aparelho-Digestivos_teste.html", "r+", encoding="utf-8") as file1:
     texto = file1.read()
     linhas = texto.splitlines()
 
 t = dicionario.keys()
 texto =""
 for l in linhas:
-    words = re.sub(r"<.+?>", " ", l) 
-    for w in re.findall(r"\b.+?\b", words): 
-        w = w.lower()
-        if w in t:
-            l = re.sub(w, r"<a href title=" + dicionario.get(w) + r">" + w + "</a>", l)
+    palavras = re.sub(r"<.+?>", " ", l) 
+    for p in re.findall(r"\b.+?\b", palavras): 
+        p = p.lower()
+        if p in t:
+            l = re.sub(p, f"<a href title='{dicionario.get(p)}'>{p}</a>", l)
     texto += l+"\n"
+with open("LIVRO-Doenças-do-Aparelho-Digestivos_teste.html", "w",  encoding="utf-8") as file1:
+    file1.write(texto)
 
-file1.write(texto)
 
-
-#dá este erro:
+#foram usados ficheiros de teste de json e html mais pequenos que os originais para ser possível testar, utilizando os originais dava este erro:
 #Traceback (most recent call last):
   #File "c:\Users\Asus\Desktop\UM\4º ano\2º semestre\PLN2\PLN\aula4\file2.py", line 42, in <module>
     #l = re.sub(w, r"<a href title=" + dicionario.get(w) + r">" + w + "</a>", l)
